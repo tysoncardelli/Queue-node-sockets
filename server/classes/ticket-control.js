be484d4faa5bf64e000e018b885351c867fd08af
Tyson Cardelli <tysoncardelli@gmail.com>
2020-08-31 17:55:39 +0100

    advance

--- server/classes/ticket-control.js
+++ server/classes/ticket-control.js
@@ -1,13 +1,21 @@
 const fs = require('fs')
+class Ticket {
+    constructor(numero,escritorio){
+        this.numero = numero;
+        this.escritorio = escritorio;
+    }
+}
 class TicketControl {
 
     constructor(){
         this.ultimo = 0;
         this.hoy = new Date().getDate();
+        this.tickets = [];
         let data = require('../data/data.json');
         console.log(data);
         if(data.hoy ===this.hoy){
             this.ultimo = data.ultimo;
+            this.tickets = data.tickets;
         }
         else{
             this.reiniciarConteo();
@@ -15,21 +23,30 @@ class TicketControl {
     }
 
     reiniciarConteo(){
+
        this.ultimo = 0;
+       this.tickets =[]
        console.log("se ha inicializado el sistema")
         this.grabarArchivo();
     }
 
     siguiente (){
         this.ultimo += 1;
+        let ticket = new Ticket(this.ultimo, null);
+        this.tickets.push(ticket);
         this.grabarArchivo();
+        return `Ticket ${this.ultimo}`;
+    }
+
+    getUltimoTicket(){
         return `Ticket ${this.ultimo}`;
     }
 
     grabarArchivo() {
         let jsonData = {
             ultimo: this.ultimo,
-            hoy: this.hoy
+            hoy: this.hoy,
+            tickets: this.tickets
         };
 
         let jsonDataString = JSON.stringify(jsonData);
--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -4,40 +4,17 @@ const {TicketControl} = require('../classes/ticket-control')
 const ticketControl = new TicketControl();
 io.on('connection', (client) => {
 
-    console.log('Usuario conectado');
+    client.on('siguienteTicket', (data,callback)=>{
+        let siguiente = ticketControl.siguiente();
+        console.log(siguiente)
+        callback(siguiente)
 
-    client.emit('enviarMensaje', {
-        usuario: 'Administrador',
-        mensaje: 'Bienvenido a esta aplicación'
-    });
+    })
+    client.emit('estadoActual',{
+        actual: ticketControl.getUltimoTicket()
+    })
 
-
-
-    client.on('disconnect', () => {
-        console.log('Usuario desconectado');
-    });
-
-    // Escuchar el cliente
-    client.on('enviarMensaje', (data, callback) => {
-
-        console.log(data);
-
-        client.broadcast.emit('enviarMensaje', data);
-
-
-        // if (mensaje.usuario) {
-        //     callback({
-        //         resp: 'TODO SALIO BIEN!'
-        //     });
-
-        // } else {
-        //     callback({
-        //         resp: 'TODO SALIO MAL!!!!!!!!'
-        //     });
-        // }
-
-
-
-    });
+    //emitirEventoEstadoactual
+    
 
 });
