ba46511c3a29e7f755238f5d453104d58b6526fa
Tyson Cardelli <tysoncardelli@gmail.com>
2020-08-31 17:59:50 +0100

    tickes control

--- server/classes/ticket-control.js
+++ server/classes/ticket-control.js
@@ -11,11 +11,14 @@ class TicketControl {
         this.ultimo = 0;
         this.hoy = new Date().getDate();
         this.tickets = [];
+        this.ultimos4 = [];
+
         let data = require('../data/data.json');
         console.log(data);
         if(data.hoy ===this.hoy){
             this.ultimo = data.ultimo;
             this.tickets = data.tickets;
+            this.ultimos4 = data.ultimos4;
         }
         else{
             this.reiniciarConteo();
@@ -25,7 +28,9 @@ class TicketControl {
     reiniciarConteo(){
 
        this.ultimo = 0;
-       this.tickets =[]
+       this.tickets =[];
+       this.ultimos4 = [];
+
        console.log("se ha inicializado el sistema")
         this.grabarArchivo();
     }
@@ -42,11 +47,30 @@ class TicketControl {
         return `Ticket ${this.ultimo}`;
     }
 
+    atenderTicket(escritorio){
+        if(this.tickets.length===0){
+            return 'No hay tickets';
+        }
+        let numeroTicket = this.tickets[0].numero;
+        this.tickets.shift();
+        let atenderTicket = new Ticket(numeroTicket,escritorio);
+        this.ultimos4.unshift(atenderTicket);
+        if(this.ultimos4.length>4){
+            this.ultimos4.splice(-1,1) //borra el ultimo elemento. 
+        }
+        console.log('Ultimos 4');
+        console.log(this.ultimos4);
+
+        this.grabarArchivo();
+        return atenderTicket;
+    }
+
     grabarArchivo() {
         let jsonData = {
             ultimo: this.ultimo,
             hoy: this.hoy,
-            tickets: this.tickets
+            tickets: this.tickets,
+            ultimos4: this.ultimos4
         };
 
         let jsonDataString = JSON.stringify(jsonData);
