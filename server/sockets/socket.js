91dfa6f8bbfb59f0a7a65861d1336ab52b92811c
Tyson Cardelli <tysoncardelli@gmail.com>
2020-09-01 14:26:13 +0100

    finish

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -11,7 +11,24 @@ io.on('connection', (client) => {
 
     })
     client.emit('estadoActual',{
-        actual: ticketControl.getUltimoTicket()
+        actual: ticketControl.getUltimoTicket(),
+        ultimos4: ticketControl.getUltimos4()
+    })
+
+    client.on('atenderTicket', (data, callback)=>{
+        if(!data.escritorio){
+            return callback({
+                err: true,
+                mensaje: 'El escritorio es necesario'
+            });
+        }
+
+        let atenderTicket = ticketControl.atenderTicket(data.escritorio);
+
+        callback(atenderTicket);
+
+        //actualizar o notificar cambios en los ultimos 4
+
     })
 
     //emitirEventoEstadoactual
